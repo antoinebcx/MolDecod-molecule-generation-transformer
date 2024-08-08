00d8887de7014151cfe54c970a67dced3a6e4593
tonito9 <antoine.branchoux@choco.com>
2024-08-08 13:45:26 +0200

    enhance molecular properties function

--- utils/properties.py
+++ utils/properties.py
@@ -7,10 +7,26 @@ def calculate_properties(smiles):
         return None
     
     properties = {
-        "Molecular Weight": Descriptors.MolWt(mol),
-        "LogP": Descriptors.MolLogP(mol),
+        "Molecular Weight": round(Descriptors.MolWt(mol), 2),
+        "LogP": round(Descriptors.MolLogP(mol), 2),
         "Number of H-Bond Donors": Descriptors.NumHDonors(mol),
         "Number of H-Bond Acceptors": Descriptors.NumHAcceptors(mol),
+        "Bertz Complexity Index": round(Descriptors.BertzCT(mol), 2),
+        "Molecular Complexity Score": round(Descriptors.MolComplexity(mol), 2),
+        "Hall-Kier Alpha Value": round(Descriptors.HallKierAlpha(mol), 4),
+        "Kappa1 Shape Index": round(Descriptors.Kappa1(mol), 4),
+        "Kappa2 Shape Index": round(Descriptors.Kappa2(mol), 4),
+        "Kappa3 Shape Index": round(Descriptors.Kappa3(mol), 4),
+        "Balaban J Index": round(Descriptors.BalabanJ(mol), 4),
+        "Molar Refractivity": round(Descriptors.MolMR(mol), 2),
+        "Maximum Partial Charge": round(Descriptors.MaxPartialCharge(mol), 4),
+        "Minimum Partial Charge": round(Descriptors.MinPartialCharge(mol), 4),
+        "Topological Polar Surface Area": round(Descriptors.TPSA(mol), 2),
+        "Approximate Surface Area": round(Descriptors.LabuteASA(mol), 2),
+        "Number of Rotatable Bonds": Descriptors.NumRotatableBonds(mol),
+        "Quantitative Estimate of Drug-likeness": round(Descriptors.QED(mol), 4),
+        "Number of Saturated Rings": Descriptors.NumSaturatedRings(mol),
+        "Number of Aromatic Rings": Descriptors.NumAromaticRings(mol)
     }
     return properties
 
